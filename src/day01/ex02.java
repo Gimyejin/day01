bdba45c564f2b0bb3e3e1c34893900646f5d4de9
kim <rla5378@naver.com>
2021-07-23 11:31:47 +0900

    Ex02 생성

---
+++ src/day01/ex02.java
@@ -0,0 +1,13 @@
+package day01;
+
+public class ex02 {
+	public static void main(String[] args) {
+		/*
+		 \n : 엔터와 비슷
+		 \t : 탭크기 만큼 오른쪽으로 이동(윈도우에서는 대략 8칸정도)
+		 \" : "(큰따음표) 표시 (일반 문자로 사용하고자 할때)
+		 \\ : \(역슬러쉬) 표현할 때 사용
+		  
+		 */
+	}
+}
