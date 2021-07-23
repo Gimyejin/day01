2edb58dfb289a318a4a46d1f2e2ff675b24563d9
kim <rla5378@naver.com>
2021-07-23 11:19:26 +0900

    주석 방법

--- src/day01/EX01.java
+++ src/day01/EX01.java
@@ -3,9 +3,14 @@ package day01;
 public class EX01 {
 
 	public static void main(String[] args) {
+		//sysout은 괄호안의 내용을 출력하는 기능
+		// ;은 문장의 끝을 알리는 용도
+		/* 문단 주석 */
 		System.out.println("안녕하세요");
 		System.out.println("java 시간입니다");
 
+		System.out.print("안녕하세요");
+		System.out.print("java 시간입니다.");
 	}
 
 }
